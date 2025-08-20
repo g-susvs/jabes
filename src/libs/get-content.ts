d3836f7ab28f6986369300f1af2a6975984f7019
g-susvs <jesus.savs19@gmail.com>
2025-08-20 10:28:59 -0500

    refactor: 💡 get content

--- src/libs/get-content.ts
+++ src/libs/get-content.ts
@@ -1,7 +1,8 @@
-import fs from "node:fs";
+import { promises as fs } from "fs";
+
 
 export const getContent = async (page: string) => {
-    const content = fs.readFileSync("./src/locales/es/content.json", "utf-8");
+    const content = await fs.readFile(process.cwd() +"/src/locales/es/content.json", "utf8");
     const contentParsed = JSON.parse(content) as { [key: string]: object };
     return contentParsed[page];
 }
