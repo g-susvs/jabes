752170cda6457297e52420c759ea47d937669813
g-susvs <jesus.savs19@gmail.com>
2025-11-25 23:22:59 -0500

    feat: 🎸 add modals to edit and delete category

--- src/modules/private/categories/components/categories-table/index.tsx
+++ src/modules/private/categories/components/categories-table/index.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { clsx } from "@/libs/clsx";
+import { Button } from "@/shared/components/button";
+import { Modal } from "@/shared/components/modal";
 import {
   Table,
   TableBody,
@@ -13,6 +17,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { SlOptionsVertical } from "react-icons/sl";
 
 const category = {
@@ -21,13 +27,33 @@ const category = {
   active: true,
 };
 
-interface IProps{
-    className?: string
+interface IEditCategory {
+  name: string;
+  active: boolean;
 }
 
-export const CategoriesTable = ({className}: IProps) => {
+interface IProps {
+  className?: string;
+}
+
+export const CategoriesTable = ({ className }: IProps) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const { handleSubmit, register } = useForm<IEditCategory>({
+    defaultValues: {
+      name: "",
+      active: true,
+    },
+  });
+
+  const onSubmit: SubmitHandler<IEditCategory> = (data) => {
+    console.log(data);
+  };
+
   return (
-       <div className={clsx("w-full", className)}>
+    <>
+      <div className={clsx("w-full", className)}>
         <div className="overflow-x-auto">
           <Table>
             <TableHeader>
@@ -51,13 +77,19 @@ export const CategoriesTable = ({className}: IProps) => {
                           </DropdownMenuTrigger>
                           <DropdownMenuContent
                             align="end"
-                            className="shadow text-zinc-600 bg-white boder-2 border-zinc-600 w-[100px]"
+                            className="text-zinc-600 bg-white boder-2 border-zinc-600 w-[100px] shadow-2xl"
                           >
-                            <DropdownMenuItem className="p-2 hover:bg-zinc-50">
-                              Edit
+                            <DropdownMenuItem
+                              className="p-2 hover:bg-zinc-200 hover:cursor-pointer"
+                              onClick={() => setIsEditModalOpen(true)}
+                            >
+                              Editar
                             </DropdownMenuItem>
-                            <DropdownMenuItem className="text-red-600 p-2 hover:bg-zinc-50">
-                              Delete
+                            <DropdownMenuItem
+                              className="text-red-600 p-2 hover:bg-zinc-200 hover:cursor-pointer"
+                              onClick={() => setIsDeleteModalOpen(true)}
+                            >
+                              Elimar
                             </DropdownMenuItem>
                           </DropdownMenuContent>
                         </DropdownMenu>
@@ -70,5 +102,61 @@ export const CategoriesTable = ({className}: IProps) => {
           </Table>
         </div>
       </div>
-  )
-}
+
+      <Modal isOpen={isEditModalOpen} onClose={() => setIsEditModalOpen(false)}>
+        <h3 className="text-2xl mb-4">Editar categoría</h3>
+        <form
+          className="flex flex-col justify-start items-start gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            placeholder="Ingresa el nombre de la categoría"
+            className="border p-2 rounded w-full"
+            {...register("name")}
+          />
+          <label>
+            <input type="checkbox" {...register("active")} />
+            <span className="w-auto"> Activar categoría</span>
+          </label>
+          <div className="flex justify-between gap-2 w-full">
+            <Button
+              onClick={() => setIsEditModalOpen(false)}
+              className="bg-red-800 hover:bg-red-900 transition-all"
+            >
+              Cancelar
+            </Button>
+            <Button type="submit" onClick={() => {}}>
+              Editar
+            </Button>
+          </div>
+        </form>
+      </Modal>
+      <Modal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+      >
+        <h3 className="text-2xl mb-4">
+          ¿Estas seguro de eliminar la categoría?
+        </h3>
+        <div
+          className="flex flex-col justify-start items-start gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <p>Se eliminará permanentemente</p>
+
+          <div className="flex justify-between gap-2 w-full">
+            <Button
+              onClick={() => setIsDeleteModalOpen(false)}
+              className="bg-red-800 hover:bg-red-900 transition-all"
+            >
+              Cancelar
+            </Button>
+            <Button type="button" onClick={() => console.log("Eliminar")}>
+              Si, estoy seguro
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
